d118e3962c53a5b8a1b3aa9c694818e9f41608e4
Mahmoud Ahmed <hodavip2000@gmail.com>
2025-07-13 15:38:46 +0300

    second task

--- app.js
+++ app.js
@@ -1,115 +1,93 @@
-const fs = require("fs");
-fs.writeFileSync("data.txt", "mahmoud")
-// console.log(fs.readFileSync("data.txt").toString())
-fs.appendFileSync("data.txt", " + NaNaPoku")
-// console.log(fs.readFileSync("data.txt").toString())
+const request = require('request')
 
-const name = require("./data.js");
-// console.log(name.sname)
+// const url = "http://api.weatherapi.com/v1/current.json?key=c3f23a5568884458a71143951251207&q=Sudan&aqi=no"
 
-const validator = require("validator")
-// console.log(validator.isEmail("hodavip2000@gmail.com"))
-// console.log(process.argv)
-// const command = process.argv[2];
-// if(command === "add")
-//     console.log("You Added an Item")
-// else if (command === "delete")
-//     console.log("You Deleted an Item")
-// else 
-//     console.log("ERROR")
-const yargs = require("yargs");
-const { type } = require("os");
-const { json } = require("stream/consumers");
-const data = require("./data.js");
+// request({url , json : true}, (error, response) => {
+//     if(error){
+//         console.log("ERROR YA AAAAAAAAAAAAAAA")
+//     }
+//     else if(response.body.error) {
+//         console.log(response.body.error.message)
+//     }
+//     else {
+//         console.log(response.body.location.name, response.body.current.condition.text)
+//     }
 
-yargs.command({
-    command : "add",
-    describe : "to add an item",
-    builder : {
-        fname : {
-            describe : "this is the first name desc in add command",
-            demandOption : true,
-            type : "string"
-        },
-        lname : {
-            describe : "this is the last name desc in add command",
-            demandOption : true,
-            type : "string"
+// })
+
+
+/////////////////////////////////////////////////////////////
+
+// const request = require('request')
+// const url = "http://api.weatherstack.com/current?access_key=292bcfaa075aaab64ee707e65c6e1d67&query="
+
+// request({url , json : true}, (error, response) => {
+
+//     if(error){
+//         console.log("ERROR YA AAAAAAAAAAAAAAA")
+//     }
+//     else if(response.body.error) {
+//         console.log(response.body.error.message)
+//     }
+//     else {
+//         console.log(response.body.location.name, response.body.current.condition.text)
+//     }
+// })
+
+
+////////////////////////////////////////////////////////////
+
+const geocode = (address , callback) => {
+    const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoibWFobW91ZDkxIiwiYSI6ImNtZDBmcG9mYzB4ejkya3EyZTFpY2lidXkifQ.u5kc6btmU_uLvBubJd10iw`;
+
+    request({ url: geocodingUrl, json: true }, (error, response) => {
+        if(error){
+            callback("error yyyyyyyyyyyy" , undefined)
         }
-    },
-    handler : (x) => {
-        data.addPerson(x.fname, x.lname, x.age, x.city,x.id)
-    }
-})
-
-yargs.command({
-    command : "delete",
-    describe : "to delete an item",
-    builder : {
-        id : {
-            describe : "id of item",
-            demandOption : true,
-            type : "string"
+        else if (response.body.message) {
+            callback(response.body.message, undefined)
         }
-    },
-    handler : (x) => {
-        data.deleteData(x.id)
-    }
-})
-
-
-yargs.command({
-    command : "read",
-    describe : "to read an item",
-    builder : {
-        id : {
-            describe : "id of item",
-            demandOption : true,
-            type : "string"
+        else if (response.body.features.length == 0) {
+            callback("Unable to find location" , undefined)
         }
-    },
-    handler : (x) => {
-        data.readData(x.id)
-    }
-})
-
-
-yargs.command({
-    command : "readAll",
-    describe : "to read all data",
-    builder : {
-        
-    },
-    handler : () => {
-        data.readAllData()
-    }
-})
-
-yargs.command({
-    command : "deleteAll",
-    describe : "to delete all data",
-    builder : {
-        
-    },
-    handler : () => {
-        data.deleteAllData()
-    }
-})
-
-yargs.command({
-    command : "list",
-    describe : "to read the full name and the city",
-    builder : {
-        id : {
-            describe : "id of item",
-            demandOption : true,
-            type : "string"
+        else {
+            callback(undefined , {
+                longtitude : response.body.features[0].center[0],
+                latitude : response.body.features[0].center[1]
+            })
         }
-    },
-    handler : (x) => {
-        data.list(x.id)
-    }
+    });
+}
+let country = process.argv[2]
+geocode(country , (error, data) => {
+    console.log("ERROR : " , error)
+    console.log("Data : " , data)
+    
+    
+    forecast(data.latitude  , data.longtitude , (error, data) => {
+        console.log("ERROR : " , error)
+        console.log("Data : " , data)
+    })
 })
 
 
-console.log(yargs.argv)
+const forecast = (latitude, longtitude , callback) => {
+    const url = "http://api.weatherapi.com/v1/current.json?key=c3f23a5568884458a71143951251207&q=" + latitude + "," + longtitude
+
+    request({url , json : true}, (error, response) => {
+        if(error){
+            callback("error yyyyyyyyyyyy" , undefined)
+        }
+        else if(response.body.error) {
+            callback(response.body.error.message, undefined)
+        }
+        else {
+            callback(undefined , response.body.location.name, response.body.current.condition.text)
+        }
+
+    })
+}
+
+
+
+
